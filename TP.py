be47a359ca4c6aa05ac6e29e7c35ec44dc23c857
JoacoAco <128651614+JoacoAco@users.noreply.github.com>
2023-05-25 19:11:57 -0300

    Fibonacci.py

--- TP.py
+++ TP.py
@@ -1,19 +1,9 @@
-#Dada una lista de N números x, indicar si la misma viene ordenada ascendentemente#
-N = int(input("Ingrese el tamaño de la lista: "))
-X = [None] * (N+1)
-I = 1
-Anterior = 0
-B = 0
-while I <= N:
-    X[I] = int(input(f"Ingrese los {N} valores: "))
-    A = X[I]
-    if A >= Anterior:
-        Anterior = X[I]
-        I = I + 1
-    elif A < Anterior:
-        B = 1
-        Anterior = X[I]
-if B == 0:
-    print("La lista está ordenada ascendentemente")
-elif B == 1:
-    print("La lista no está ordenada ascendentemente")
+k = int(input("Ingrese la cantidad de términos de Fibonacci: "))
+F = [None] * (k)
+I = 2
+F[0] = 1
+F[1] = 1
+while I < k:
+    F[I] = F[I-1] + F[I-2]
+    I = I + 1
+print(F, "Es el conjunto de los",k,"primeros números de Fibonacci")
